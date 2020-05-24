f41cac0f3b1077704dbbb68693a10711fc66c7d4
PengMeng <383867421@qq.com>
2020-05-24 16:15:26 +0800

    deploy

---
+++ precache-manifest.15d88922a3876c2590cfeddda1cb06b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dd6c72095182912507849817118623",
+    "url": "/morney-react-website/index.html"
+  },
+  {
+    "revision": "6f0ef7220166dc660eec",
+    "url": "/morney-react-website/static/css/main.e5843d78.chunk.css"
+  },
+  {
+    "revision": "fb19d0ba71b4a51d0ff5",
+    "url": "/morney-react-website/static/js/2.402c479b.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-react-website/static/js/2.402c479b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0ef7220166dc660eec",
+    "url": "/morney-react-website/static/js/main.3167db72.chunk.js"
+  },
+  {
+    "revision": "4f7fcab684a1b38d7039",
+    "url": "/morney-react-website/static/js/runtime-main.2b4c01eb.js"
+  }
+]);
